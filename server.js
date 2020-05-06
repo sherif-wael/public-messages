7e08e9be83b9f2434af27652f10e84432aa7d6ad
Sherif Wael <sherifwael80@gmail.com>
2020-05-06 08:02:08 +0200

    Initial Commit

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"))
+    //ashdkasd
 })
 
 
