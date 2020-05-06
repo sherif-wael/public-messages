66b3a42feb67f77fd600a73a5318a664e89d131a
Sherif Wael <sherifwael80@gmail.com>
2020-05-06 07:45:05 +0200

    Initial Commit

--- server.js
+++ server.js
@@ -31,26 +31,13 @@ app.use("/api/profile", require("./routes/api/profile"))
 app.use("/api/message", require("./routes/api/message"))
 
 //serving static files
-// app.use(express.static("client/build"))
+app.use(express.static("./client/build"));
 
-// app.use("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"))
-//     if(process.env.NODE_ENV === "production"){
-//         res.sendFile(path.join(__dirname, "./client/build/index.html"))
-//     }else{
-//         res.sendFile(path.join(__dirname, "./client/public/index.html"))
-//     }
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
 
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
-  
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("server is listening to port 5000"))
